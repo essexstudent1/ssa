18e9d4a94d4906cbe390747aabbb0ef619946b32
Kate Wood <94301901+essexstudent1@users.noreply.github.com>
2022-09-01 17:35:17 -0400

    Update lock.py

--- src/lock.py
+++ src/lock.py
@@ -35,7 +35,7 @@ def on_message(client, userdata, message):
     if message.payload.decode() == "Lock":
         client.publish("LOCK_STATUS", "Locked", qos=2)
         MyLock.lock()
-    elseif message.payload.decode() == "Unlock":
+    elif message.payload.decode() == "Unlock":
         client.publish("LOCK_STATUS", "Unlocked", qos=2)
         MyLock.unlock()
     else:

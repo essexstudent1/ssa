f4114c695fe43731f1fb2a60055d33633e8e740e
Ags Angelides <93327702+AgsAngelides@users.noreply.github.com>
2022-09-08 13:51:52 +0100

    Update hub.py

--- docker/hub/hub.py
+++ docker/hub/hub.py
@@ -1,25 +1,25 @@
 """The controller hub script which connects to the MQTT broker and publishes the user's input to the 'LOCK' topic. It also subscribes to the 'LOCK_STATUS' topic
 on the MQTT broker which is populated by the smart lock"""
-import paho.mqtt.client as mqtt
-from cryptography.fernet import Fernet
-import hashlib, os
-
 from threading import Thread
 from queue import Queue
-import time
+from cryptography.fernet import Fernet
+import time, hashlib, os
+import paho.mqtt.client as mqtt
 
-# Define the encryption class to encrypt and decrypt the hub wakeup password
 class Encryption:
+    """Define the encryption class to encrypt and decrypt the hub wakeup password"""
     def __init__(self):
         self.file = "wake_word.txt"
         self.key_file = "filekey.key"
 
     def generate_key(self):
+        """Generates an encryption key and stores it in a file"""
         key = Fernet.generate_key()
         with open(self.key_file, "wb") as filekey:
             filekey.write(key)
 
     def encrypt_wake_word(self):
+        """Encrypts the wake word entered by using the encryption key from the file, stores the encrypted wake word in a file"""
         with open(self.key_file, "rb") as filekey:
             key = filekey.read()
         fernet = Fernet(key)
@@ -30,6 +30,7 @@ class Encryption:
             encrypted_file.write(encrypted)
 
     def decrypt_wake_word(self):
+        """Decrypts the wake word entered by the user using the encryption key from the file"""
         with open(self.key_file, "rb") as filekey:
             key = filekey.read()
         fernet = Fernet(key)
@@ -38,18 +39,13 @@ class Encryption:
         decrypted = fernet.decrypt(encrypted).decode("utf-8")
         return decrypted
 
-# Define the WakeUpHub class control the hub wake functions
 class WakeUpHub:
+    """A class used to wake up the hub, create a wake word and authenticate it"""
     def __init__(self):
         self.file = "wake_word.txt"
 
-    def check_wake_exists(self):
-        if os.stat(self.file).st_size == 0:
-            self.create_wake_word()
-        else:
-            return True
-
     def auth_wake_word(self, word):
+        """Decrypts the wake word set up and stored in a file and compares it to the wake word entered by the user"""
         encr = Encryption()
         stored_word = encr.decrypt_wake_word()
         if word == stored_word:
@@ -58,6 +54,7 @@ class WakeUpHub:
             return False
 
     def create_wake_word(self):
+        """Asks the user to input wake word, encrypts it and stores it in a file"""
         with open(self.file, "w") as f:
             wake_word = input("Please create a custom wake word by entering it below\n>>>> ")
             encr = Encryption()
@@ -67,6 +64,7 @@ class WakeUpHub:
             encr.encrypt_wake_word()
 
     def wake_up(self):
+        """If the wake word file is not 0 (wake word exists), asks the user to input the wake word. Otherwise, asks the user to create a wake word"""
         if os.stat(self.file).st_size == 0:
             self.create_wake_word()
             return self.wake_up()
@@ -74,22 +72,20 @@ class WakeUpHub:
             wake_word = input("Please enter wake word below:\n>>>>> ")
             return self.auth_wake_word(wake_word)
 
-
-# Advises the user whether they are connected to the MQTT broker or not.
 def on_connect(client, userdata, flags, rc):
+    """Advises the user whether they are connected to the MQTT broker or not"""
     if rc == 0:
         print("Connected to MQTT Broker")
         client.subscribe("LOCK_STATUS", qos=2)
     else:
         print("Failed to connect, return code %d\n", rc)
 
-
-# Producer activity - reads the data from the MQTT server and puts it into the message queue to be processed by the consumer thread
 def on_message(client, userdata, message):
+    """Producer activity - reads the data from the MQTT server and puts it into the message queue to be processed by the consumer thread"""
     MsgQ.put(message.payload.decode())
 
-# Define the consumer thread to read messages from the message queue
 def consumer():
+    """Define the consumer thread to read messages from the message queue"""
     while True:
         msg = MsgQ.get()
         if msg == "Locked":
@@ -100,9 +96,8 @@ def consumer():
             print("Unknown command recieved")
         MsgQ.task_done()
 
-
-# Publishes the user's request to lock/unlock to the MQTT broker. If the input is invalid, it asks the user to enter a valid option or allows them to exit.
 def user_action(user_input):
+    """Publishes the user's request to lock/unlock to the MQTT broker. If the input is invalid, it asks the user to enter a valid option or allows them to exit"""
     if user_input.lower() == "lock":
         client.publish("LOCK", "Lock", qos=2)  # Publishes 'Lock' to the MQTT broker topic 'LOCK'
     elif user_input.lower() == "unlock":
@@ -115,22 +110,15 @@ def user_action(user_input):
         user_action(action)
 
 def broker_auth():
+    """Opens the password file where the controller hub user name and the password are stored for accessing the MQTT broker"""
     with open("hub_passwordfile.txt") as f:
         lines = f.readlines()
         user = lines[0].strip()
         passwd = lines[1].strip()
     return user, passwd
 
-
-def check_wake_exists():
-    with open("wake_word.txt") as f:
-        lines = f.readlines()
-        if len.lines[0] == 0:
-            wake_word = input("Please create a custom wake word by entering it below\n>>>> ")
-
-
-# asks user for their input, calls the function to verify the user input
 def user_interface(wakeuptest):
+    """Asks user for their input, calls the function to verify the user input"""
     wake = WakeUpHub()
     if wakeuptest == False:
         print("What would you like to do?\n-Unlock\n-Lock\n-Exit\nPlease type your request below\n>>>")
@@ -152,31 +140,26 @@ t = Thread(target=consumer)
 t.daemon = True
 t.start()
 
-# Identifying online broker location - Eclipse mosquito
-# mqttBroker ="mqtt.eclipseprojects.io"
-mqttBroker = "mqtt.aliahmed.app"
+mqttBroker = "mqtt.aliahmed.app" # Identifies the MQTT broker location
 
-# Setting up a new client and telling it to connect to the broker
-client = mqtt.Client("Hub-984323")
-client.username_pw_set(broker_auth()[0], broker_auth()[1])
-client.connect(mqttBroker)
+client = mqtt.Client("Hub-984323") # Sets up the new client
+client.username_pw_set(broker_auth()[0], broker_auth()[1]) # Passes the client credentials to the MQTT broker
+client.connect(mqttBroker) # Tells the client to connect to the broker
 
 client.on_connect = on_connect
 client.on_message = on_message
 
-wakeuptestcomplete = False
+wakeup_test_complete = False # Sets the initial hub state to not awake
+
+# Defines a loop which allows the user to lock/unlock the door without having to enter the wake word each time if the wake word has been authenticated
 while True:
-    # client.on_connect = on_connect
-    if wakeuptestcomplete == False:
+    if wakeup_test_complete == False:
         user_interface(True)
-        wakeuptestcomplete = True
+        wakeup_test_complete = True
         client.loop_start()
     else:
-        time.sleep(1) # wait for thread to update status on screen
+        time.sleep(1) # Wait for thread to update status on screen
         user_interface(False)
         client.loop_start()
 
 client.loop_forever()
-
-
-

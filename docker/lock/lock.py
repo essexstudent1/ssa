795ca9a7f0d04cf7f197f42e7010ebac6dfb8949
SherelleGarwood <93330422+SherelleGarwood@users.noreply.github.com>
2022-09-11 19:38:35 +0100

    Update lock.py

--- docker/lock/lock.py
+++ docker/lock/lock.py
@@ -1,35 +1,34 @@
 """The smart lock script which subscribes to the MQTT broker 'LOCK' topic, triggers the doors to be locked and then publishes to the topic 'LOCK_STATUS' on the
 MQTT broker"""
-import paho.mqtt.client as mqtt
-
 from threading import Thread
 from queue import Queue
-
+from datetime import datetime
+import random
+import paho.mqtt.client as mqtt
 
 # Define the Smartlock class
 class SmartLock:
+    """smart lock class, lock or unlock option"""
     def __init__(self):
         # insert code here to interface with hardware and read the initial status of the lock.
-        # since we don't have real hardware, we are going to assume that the lock always initializes to unlocked
+        # assuming that the lock always initializes to unlocked
         self.status = 0
 
     def lock(self):
-        # insert code here to interface with hardware and secure the lock
-        # ideally we would have error handling here in case the lock fails to secure. we are assuming the locking mechanism always works.
+        """interact with hardware and secure the lock"""
         self.status = 1  # status = 1 = locked
         client.publish("LOCK_STATUS", "Locked", qos=2)
         print("The door is locked")
 
     def unlock(self):
-        # insert code here to interface with hardware and open the lock
-        # ideally we would have error handling here in case the lock fails to open. we are assuming the unlocking mechanism always works.
+        """insert code here to interact with hardware and open the lock"""
         self.status = 0  # status = 0 = unlocked
         client.publish("LOCK_STATUS", "Unlocked", qos=2)
         print("The door is unlocked")
 
 
-# Pulls the broker login details from a password file
 def broker_auth():
+    """Pulls the broker login details from a password file"""
     with open("lock_passwordfile.txt") as f:
         lines = f.readlines()
         user = lines[0].strip()
@@ -37,8 +36,8 @@ def broker_auth():
     return user, passwd
 
 
-# Advises the user whether they are connected to the MQTT broker or not.
 def on_connect(client, userdata, flags, r_c):
+    """ Advises the user whether they are connected to the MQTT broker or not"""
     if r_c == 0:
         print("Connected to MQTT Broker")
         client.subscribe("LOCK", qos=2)
@@ -46,13 +45,15 @@ def on_connect(client, userdata, flags, r_c):
         print("Failed to connect, return code %d\n", r_c)
 
 
-# Producer activity - reads the data from the MQTT server and puts it into the message queue to be processed by the consumer thread
+
 def on_message(client, userdata, message):
+    """ Reads data from the MQTT server and puts into message queue to be processed"""
     MsgQ.put(message.payload.decode())
 
 
-# Define the consumer thread to read the message queue and call the appropriate smartlock function
+
 def consumer():
+    """Consumer thread to read the message queue and call the appropriate smartlock function"""
     while True:
         msg = MsgQ.get()
         if msg == "Lock":
@@ -60,13 +61,12 @@ def consumer():
         elif msg == "Unlock":
             MyLock.unlock()
         else:
-            print("Unknown command recieved")
+            print("Unknown command received")
         MsgQ.task_done()
 
-# Generates a unique number using current date/time and random number
+
 def unique_number():
-    from datetime import datetime
-    import random
+    """Generates a unique number using current date/time and random number"""
     n1 = datetime.now()
     return n1.strftime("%Y%m%d%H%M%S") + str(random.randint(0,9999))
 
@@ -84,7 +84,7 @@ t.start()
 # MQTT broker location, currently set to localhost
 MQTT_BROKER = "mqtt.aliahmed.app"
 
-# Setting up a new client to connect to the broker to subscribe to the lock status and connecting this client to the broker
+# New client to connect to the broker to subscribe to the lock status
 client = mqtt.Client("Lock-984323" + unique_number())
 client.username_pw_set(broker_auth()[0], broker_auth()[1])
 client.connect(MQTT_BROKER)
@@ -95,4 +95,3 @@ client.on_message = on_message
 
 # Producer thread loops continually looking for messages from the MQTT server
 client.loop_forever()
-
